2c52685af6eba0c024714ee785f9ba78f9894510
devdrowsy <devdrowsy@gmail.com>
2022-01-23 21:31:54 -0500

    change title

--- app/root.tsx
+++ app/root.tsx
@@ -14,7 +14,7 @@ export function links() {
 }
 
 export const meta: MetaFunction = () => {
-  return { title: 'New Remix App' };
+  return { title: 'The Drowsy Dev' };
 };
 
 export default function App() {
